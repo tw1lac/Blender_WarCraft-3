d3df369756790bf4b1fb98c1746077d230c42e44
tw1lac <pikofluff@gmail.com>
2020-11-26 16:14:34 +0100

    removed a line that appeared double after rebasing

--- io_scene_warcraft_3/mdx_parser/parse_geometry.py
+++ io_scene_warcraft_3/mdx_parser/parse_geometry.py
@@ -14,6 +14,7 @@ def parse_geometry(data):
     print(chunkId)
     vertexCount = r.getf('<I')[0]
     print(vertexCount)
+
     for _ in range(vertexCount):
         vertexPositionX, vertexPositionY, vertexPositionZ = r.getf('<3f')
         # print(vertexPositionX, ", ", vertexPositionY, ", ", vertexPositionZ)
@@ -25,14 +26,17 @@ def parse_geometry(data):
     for chunk in chunks_to_skip:
         chunkId = r.getid(chunk[0])
         count = r.getf('<I')[0]
+
         for _ in range(count):
             chunk_thing = r.getf(chunk[1])
 
     # parse
     chunkId = r.getid(constants.CHUNK_FACE)
     indicesCount = r.getf('<I')[0]
+
     if indicesCount % 3 != 0:
         raise Exception('bad indices (indicesCount % 3 != 0)')
+
     for _ in range(indicesCount // 3):
         vertexIndex1, vertexIndex2, vertexIndex3 = r.getf('<3H')
         mesh.triangles.append((vertexIndex1, vertexIndex2, vertexIndex3))
@@ -41,6 +45,7 @@ def parse_geometry(data):
     chunkId = r.getid(constants.CHUNK_VERTEX_GROUP)
     matrixGroupsCount = r.getf('<I')[0]
     matrixGroups = []
+
     for _ in range(matrixGroupsCount):
         matrixGroup = r.getf('<B')[0]
         matrixGroups.append(matrixGroup)
@@ -49,6 +54,7 @@ def parse_geometry(data):
     chunkId = r.getid(constants.CHUNK_MATRIX_GROUP)
     matrixGroupsSizesCount = r.getf('<I')[0]
     matrixGroupsSizes = []
+
     for _ in range(matrixGroupsSizesCount):
         matrixGroupSize = r.getf('<I')[0]
         matrixGroupsSizes.append(matrixGroupSize)
@@ -57,6 +63,7 @@ def parse_geometry(data):
     chunkId = r.getid(constants.CHUNK_MATRIX_INDEX)
     matrixIndicesCount = r.getf('<I')[0]
     matrixIndices = []
+
     for _ in range(matrixIndicesCount):
         matrixIndex = r.getf('<I')[0]
         matrixIndices.append(matrixIndex)
@@ -68,40 +75,44 @@ def parse_geometry(data):
     mesh.material_id = r.getf('<I')[0]
     selectionGroup = r.getf('<I')[0]
     selectionFlags = r.getf('<I')[0]
+
     if constants.MDX_CURRENT_VERSION > 800:
         lod = r.getf('<I')[0]
         lodName = r.gets(80)
+
     boundsRadius = r.getf('<f')[0]
     minimumExtent = r.getf('<3f')
     maximumExtent = r.getf('<3f')
     extentsCount = r.getf('<I')[0]
+
     for _ in range(extentsCount):
         boundsRadius = r.getf('<f')[0]
         minimumExtent = r.getf('<3f')
         maximumExtent = r.getf('<3f')
 
     # parse and ignore Texture vertex groups
-    chunkId = r.getid(constants.CHUNK_TEXTURE_VERTEX_GROUP)
-    ############################################################################
-    ################################# NOT USED #################################
-    ############################################################################
     if constants.MDX_CURRENT_VERSION > 800:
         chunkId = r.getid((constants.CHUNK_TANGENTS, constants.CHUNK_SKIN, constants.CHUNK_TEXTURE_VERTEX_GROUP))
+
         if chunkId == constants.CHUNK_TANGENTS:
             tangentSize = r.getf('<I')[0]
-            r.skip(16*tangentSize) #4 floats per tangent
+            r.skip(16*tangentSize)  # 4 floats per tangent
             chunkId = r.getid((constants.CHUNK_SKIN, constants.CHUNK_TEXTURE_VERTEX_GROUP))
+
         if chunkId == constants.CHUNK_SKIN:
             skinSize = r.getf('<I')[0]
             r.skip(skinSize)
             chunkId = r.getid(constants.CHUNK_TEXTURE_VERTEX_GROUP)
+
     else:
         chunkId = r.getid(constants.CHUNK_TEXTURE_VERTEX_GROUP)
+
     textureVertexGroupCount = r.getf('<I')[0]
 
     # parse uv-coordinates
     chunkId = r.getid(constants.CHUNK_VERTEX_TEXTURE_POSITION)
     vertexTexturePositionCount = r.getf('<I')[0]
+
     for _ in range(vertexTexturePositionCount):
         u, v = r.getf('<2f')
         mesh.uvs.append((u, 1 - v))
